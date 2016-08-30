47fa09cc499160ecf0133ad06174125559719d55
Oddur Sigurdsson <oddur@fullstackacademy.com>
2016-08-30 04:01:22 -0400

    adding minification and setting up structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,29 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
+var nested = require('postcss-nested');
+var scss = require('postcss-scss');
+var cssimport = require("gulp-cssimport");
+var rename = require("gulp-rename")
 
-gulp.task('css', function () {
+gulp.task('css', function() {
   var processors = [
     autoprefixer({browsers: ['last 1 version']}),
-    cssnano(),
+    nested
   ];
-  return gulp.src('./src/*.css')
-  .pipe(postcss(processors))
+  return gulp.src('./src/sutra.css')
+  .pipe(cssimport())
+  .pipe(postcss(processors, {syntax: scss}))
   .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('css-min', function() {
+  return gulp.src('./dist/sutra.css')
+  .pipe(postcss([cssnano()]))
+  .pipe(rename(function (path) {
+    path.dirname += "/";
+    path.basename += ".min";
+    path.extname = ".css"
+  }))
+  .pipe(gulp.dest('./dist'))
+})
